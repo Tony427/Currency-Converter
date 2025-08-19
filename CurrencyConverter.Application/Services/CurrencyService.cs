5608c89f0863438ef19567a649b2701293d2c30d
Tony Tsai <ttony427@gmail.com>
2025-08-19 19:06:19 +0800

    "Task5_CurrencyService_Update"

--- CurrencyConverter.Application/Services/CurrencyService.cs
+++ CurrencyConverter.Application/Services/CurrencyService.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using System;
 using System.Linq;
+using Microsoft.Extensions.Options;
+using CurrencyConverter.Application.Settings;
 
 namespace CurrencyConverter.Application.Services
 {
@@ -11,12 +13,13 @@ namespace CurrencyConverter.Application.Services
     {
         private readonly ICurrencyProviderFactory _currencyProviderFactory;
         private readonly ICacheService _cacheService;
-        private readonly HashSet<string> _excludedCurrencies = new HashSet<string> { "TRY", "PLN", "THB", "MXN" };
+        private readonly HashSet<string> _excludedCurrencies;
 
-        public CurrencyService(ICurrencyProviderFactory currencyProviderFactory, ICacheService cacheService)
+        public CurrencyService(ICurrencyProviderFactory currencyProviderFactory, ICacheService cacheService, IOptions<CurrencySettings> currencySettings)
         {
             _currencyProviderFactory = currencyProviderFactory;
             _cacheService = cacheService;
+            _excludedCurrencies = new HashSet<string>(currencySettings.Value.ExcludedCurrencies, StringComparer.OrdinalIgnoreCase);
         }
 
         public async Task<IEnumerable<ExchangeRate>> GetLatestExchangeRatesAsync(string baseCurrency)
