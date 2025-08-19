cec38176c6a4add18d72c5f58b8cad45e07667c5
Tony Tsai <ttony427@gmail.com>
2025-08-19 19:13:42 +0800

    "Task7_CustomLogEnrichers"

---
+++ CurrencyConverter.API/Logging/CustomLogEnrichers.cs
@@ -0,0 +1,51 @@
+using Serilog.Core;
+using Serilog.Events;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace CurrencyConverter.API.Logging
+{
+    public class ClientIpEnricher : ILogEventEnricher
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ClientIpEnricher(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+            if (httpContext == null) return;
+
+            var clientIp = httpContext.Connection.RemoteIpAddress?.ToString();
+            if (!string.IsNullOrEmpty(clientIp))
+            {
+                logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("ClientIp", clientIp));
+            }
+        }
+    }
+
+    public class UserIdEnricher : ILogEventEnricher
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserIdEnricher(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+            if (httpContext == null) return;
+
+            var userId = httpContext.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (!string.IsNullOrEmpty(userId))
+            {
+                logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("UserId", userId));
+            }
+        }
+    }
+}
